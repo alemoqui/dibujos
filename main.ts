90e9b8805f70cd7fa7b6a73cb29895e8a62ad240
alemoqui <130452678+alemoqui@users.noreply.github.com>
2023-04-17 09:37:58 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,6 +26,62 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(1000)
     }
 })
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            # # # # #
+            # . . . #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . # . . .
+            . # . . .
+            . # # # #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # . .
+            . # . . .
+            . # # # .
+            `)
+        basic.pause(1000)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # . # .
+            . # . # .
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.pause(1000)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(1000)
+    }
+})
 basic.forever(function () {
 	
 })
